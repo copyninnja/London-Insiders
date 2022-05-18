e4b6894c0ddd84c3c2e746c7e2937129abb34fbf
xiaofeng gong <xiaofenggong@pc-10-189.customer.ask4.lan>
2022-05-18 20:21:58 +0100

    Updates

---
+++ precache-manifest.fc09acb835c4b125c1fae2875f9a31e7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e959f17e5e8937dc84325aa1a4ab1f",
+    "url": "/London-Insiders/index.html"
+  },
+  {
+    "revision": "4e7193104fff3c0a1fee",
+    "url": "/London-Insiders/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "7fa67331e28ae8358af4",
+    "url": "/London-Insiders/static/css/main.e71d08eb.chunk.css"
+  },
+  {
+    "revision": "4e7193104fff3c0a1fee",
+    "url": "/London-Insiders/static/js/2.bd062c1a.chunk.js"
+  },
+  {
+    "revision": "7fa67331e28ae8358af4",
+    "url": "/London-Insiders/static/js/main.bf86a8a8.chunk.js"
+  },
+  {
+    "revision": "c31b62a7a3cd226a9f8e",
+    "url": "/London-Insiders/static/js/runtime~main.d2aeeb8b.js"
+  },
+  {
+    "revision": "4bacece8487fa7b600427b0b9659d588",
+    "url": "/London-Insiders/static/media/house.4bacece8.png"
+  },
+  {
+    "revision": "3061d5cc4eab11fff70861d996dcca30",
+    "url": "/London-Insiders/static/media/inside.3061d5cc.png"
+  },
+  {
+    "revision": "b84dc5b0e313c2f9d0388b2e2c64260d",
+    "url": "/London-Insiders/static/media/inside1.b84dc5b0.png"
+  },
+  {
+    "revision": "f133dcd58059f53f528e283b8a1158d3",
+    "url": "/London-Insiders/static/media/logo.f133dcd5.png"
+  }
+]);
